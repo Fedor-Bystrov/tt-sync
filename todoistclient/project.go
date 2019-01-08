658afd681d90bd1cab3b9bffe18bbd95118b5829
Fedor Bystrov <augustbystrov@gmail.com>
2019-01-08 18:59:06 +0300

    implement project entity

--- todoistclient/project.go
+++ todoistclient/project.go
@@ -1,5 +1,18 @@
 package todoistclient
 
-// Project is a Todoist Project entity
+import "fmt"
+
+// Project represents Todoist Project entity
+// https://developer.todoist.com/rest/v8/#projects
 type Project struct {
+	ID           uint
+	Name         string
+	Order        uint
+	Indent       uint
+	CommentCount uint `json:"comment_count"`
+}
+
+func (p Project) String() string {
+	return fmt.Sprintf("Project{id: %d, name: %s, order: %d, indent: %d, comment_count: %d}",
+		p.ID, p.Name, p.Order, p.Indent, p.CommentCount)
 }
