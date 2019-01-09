1f3447f500f15b97048e9e9fbaced22191715b09
Fedor Bystrov <augustbystrov@gmail.com>
2019-01-09 14:02:05 +0300

    implement Comment entity

--- todoistclient/comment.go
+++ todoistclient/comment.go
@@ -1,5 +1,15 @@
 package todoistclient
 
-// Comment is a Todoist Comment entity
+import "fmt"
+
+// Comment represents Todoist Comment entity
 type Comment struct {
+	TaskID    uint
+	ProjectID uint
+	Content   string
+}
+
+func (c Comment) String() string {
+	return fmt.Sprintf("Comment{task_id: %d, project_id: %d, content: %s}",
+		c.TaskID, c.ProjectID, c.Content)
 }
