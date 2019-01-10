fddbe4b14334e7c9aa7beee3ad93b34efecf6624
Fedor Bystrov <augustbystrov@gmail.com>
2019-01-10 16:43:07 +0300

    add SYNC regexp pattern to resolver

--- todoist-resolver/main.go
+++ todoist-resolver/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Fedor-Bystrov/tt-sync/todoistclient"
+	"log"
+	"os"
+	"regexp"
+)
+
+var (
+	todoistToken   = os.Getenv("TODOIST_TOKEN")
+	goEnv          = os.Getenv("GO_ENV")
+	syncPattern, _ = regexp.Compile(`^\[SYNC\]\s.+$`)
+
+	logFile *os.File
+)
+
+func init() {
+	if goEnv != "development" {
+		logFile, err := os.OpenFile("todoist-resolver.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("Fatal [Main#init] Error opening file: %v", err)
+		}
+		log.SetOutput(logFile)
+	}
+}
 
 func main() {
-	fmt.Println("works")
+	if goEnv != "development" {
+		defer logFile.Close()
+	}
+	checkVars(todoistToken)
+
+	todoistClient := todoistclient.NewClient(todoistToken, nil)
+	projects, err := todoistClient.GetProjects()
+	if err != nil {
+		log.Fatal("Fatal [Main#GetProjects] cannot fetch projects", err)
+	}
+
+	for _, p := range projects {
+		if syncPattern.MatchString(p.Name) {
+			log.Print(p)
+		}
+	}
+}
+
+func checkVars(todoistToken string) {
+	log.Print("Checking environment variables")
+	if todoistToken == "" {
+		log.Fatal("Fatal [Main#checkVars] Cannot resolve TODOIST_TOKEN env variable")
+	}
 }
