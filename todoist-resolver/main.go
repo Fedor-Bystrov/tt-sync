eb0875e4348374ab8cc87885d87fa86a6844b3e7
Fedor Bystrov <augustbystrov@gmail.com>
2019-01-10 17:09:56 +0300

    add execution time measuring

--- todoist-resolver/main.go
+++ todoist-resolver/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"time"
 )
 
 var (
@@ -16,6 +17,7 @@ var (
 )
 
 func init() {
+	defer elapsed("[Elapsed] INIT")()
 	if goEnv != "development" {
 		logFile, err := os.OpenFile("todoist-resolver.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
@@ -26,6 +28,7 @@ func init() {
 }
 
 func main() {
+	defer elapsed("[Elapsed] MAIN")()
 	if goEnv != "development" {
 		defer logFile.Close()
 	}
@@ -34,14 +37,31 @@ func main() {
 	todoistClient := todoistclient.NewClient(todoistToken, nil)
 	projects, err := todoistClient.GetProjects()
 	if err != nil {
-		log.Fatal("Fatal [Main#GetProjects] cannot fetch projects", err)
+		log.Fatal("Fatal [Main] cannot fetch projects", err)
 	}
 
+	var syncProject todoistclient.Project
 	for _, p := range projects {
 		if syncPattern.MatchString(p.Name) {
-			log.Print(p)
+			syncProject = p
 		}
 	}
+
+	tasks, err := todoistClient.GetTasks()
+	if err != nil {
+		log.Fatal("Fatal [Main] cannot fetch tasks", err)
+	}
+
+	for _, t := range tasks {
+		if t.ProjectID == syncProject.ID {
+			log.Print(t)
+		}
+	}
+}
+
+func elapsed(message string) func() {
+	start := time.Now()
+	return func() { log.Printf("%s took %v\n", message, time.Since(start)) }
 }
 
 func checkVars(todoistToken string) {
